af51b857abd56bedba8a54d2761b2491b378c8cc
Alice <ziqi.chu@mail.mcgill.ca>
2024-12-18 16:14:25 -0500

    temp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from 'node:path'
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-  }
 })
