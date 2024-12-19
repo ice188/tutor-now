776695c14d20e3c38b599635f5d9165454783071
Alice <ziqi.chu@mail.mcgill.ca>
2024-12-18 21:03:31 -0500

    decrement capacity

--- frontend/src/features/reservebooking/components/ReserveBooking.jsx
+++ frontend/src/features/reservebooking/components/ReserveBooking.jsx
@@ -83,7 +83,7 @@ export default function TutorialDetails() {
     };
   
     fetchTutorialDetails();
-  }, [decodedId]); 
+  }, [decodedId, encodedId]); 
   
   const handleConfirmReservation = async () => {
     if (!tutorial) {
