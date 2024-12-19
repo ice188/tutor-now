9e35900645fe65a1b08fd11a5a06e638b122f9c4
Alice <ziqi.chu@mail.mcgill.ca>
2024-12-18 20:51:24 -0500

    fixed

--- frontend/src/features/makebooking/components/MakeBooking.jsx
+++ frontend/src/features/makebooking/components/MakeBooking.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { addTutorial } from "../api/addTutorial";
 import { GetCourses } from "../api/getCourses";
 import { useParams } from "react-router-dom";
