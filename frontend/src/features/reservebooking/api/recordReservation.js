738edc2626c7aad59ad9df2b2d0fb63ae5d6d6bb
Yusuf Hamza <yusufh1a2m@gmail.com>
2024-12-18 22:50:06 -0500

    corrected api

--- frontend/src/features/reservebooking/api/recordReservation.js
+++ frontend/src/features/reservebooking/api/recordReservation.js
@@ -1,6 +1,6 @@
-export const UpdateTutorialCapacity= async (id) => {
+export const RecordReservation= async (uid, tid) => {
     const serverUrl = import.meta.env.VITE_SERVER_API_URL;
-    const res = await fetch(`${serverUrl}/api/tutorial/${id}/decrement-capacity`, {
+    const res = await fetch(`${serverUrl}/api/booking`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
